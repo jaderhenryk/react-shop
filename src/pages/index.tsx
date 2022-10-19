f4a337dfa5ffc20b53cd3de8300354639e03ddfb
jader henrique de paula <jader@compels.net>
2022-10-19 20:37:45 -0300

    starting to create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,59 @@
+import { useKeenSlider } from 'keen-slider/react';
+
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import Image from "next/future/image";
+import shirt1 from '../assets/camisetas/1.png';
+import shirt2 from '../assets/camisetas/2.png';
+import shirt3 from '../assets/camisetas/3.png';
+
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [ sliderRef ] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <h1>Hello World!</h1>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta One</strong>
+          <span>R$ 19,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Two</strong>
+          <span>R$ 19,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Two</strong>
+          <span>R$ 19,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Two</strong>
+          <span>R$ 19,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Two</strong>
+          <span>R$ 19,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
