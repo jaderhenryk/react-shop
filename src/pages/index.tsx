991df60d4a5d43a001d9c09a233bfd5ae33f656d
Jader Henrique de Paula <jaderhenryk@gmail.com>
2022-10-24 20:52:37 -0300

    formatting price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({ products }: HomeProps) {
             <Image src={product.imageUrl} alt="" width={520} height={480}/>
             <footer>
               <strong>{product.name}</strong>
-              <span>R$ {product.price / 100}</span>
+              <span>{product.price}</span>
             </footer>
           </Product>
         )
@@ -55,7 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
     }
   })
   return {
